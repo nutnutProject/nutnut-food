13a940eeefb99372ed590ff85e75e3537dc3babf
pierre gosselin <pierre.gosselin@orange.fr>
2019-09-03 12:39:58 +0200

    prepare login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,16 +4,26 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="login")
      */
-    public function index()
+    public function index(AuthenticationUtils $authenticationUtils)
     {
+        // Si il y a des erreurs, on les affiches
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(){}
 }
