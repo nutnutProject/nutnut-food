4fa5f17b6ed1fd9c290b2ba8d76101bcd5ca99ba
imzhai <jue.yguel@gmail.com>
2019-09-03 12:50:01 +0200

    add findCategory() in template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use App\Repository\RecetteRepository;
 use App\Entity\Recette;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +14,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(CategoryRepository $categoryRepository, RecetteRepository $recetteRepository)
     {
         $categoryRepository = $this->getDoctrine()->getRepository(Category::class);
 
         $categories = $categoryRepository->findCategory();
-
 
         return $this->render('home/home.html.twig', [
            'categories' => $categories,
