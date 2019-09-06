c78559d1dc23400c77e8fbf213011bc98de845e9
pierre gosselin <pierre.gosselin@orange.fr>
2019-09-06 13:09:44 +0200

    fix 403

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -2,17 +2,23 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
 
 class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
-    
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
-
-        return new Response("Accés refusé", 403);
+        return new RedirectResponse($this->router->generate('home'));
     }
 }
