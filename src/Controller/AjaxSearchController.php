633d71e7fe690958cb7ea78a5cdc739bafceb8cf
imzhai <jue.yguel@gmail.com>
2019-09-11 10:58:12 +0200

    add ajax

--- src/Controller/AjaxSearchController.php
+++ src/Controller/AjaxSearchController.php
@@ -12,55 +12,40 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Json;
 
 class AjaxSearchController extends AbstractController
 {
-    /**
-     * @Route("/ajax/search", name="ajax_search")
-     */
-    public function index()
-    {
-        return $this->render('ajax_search/index.html.twig', [
-            'controller_name' => 'AjaxSearchController',
-        ]);
-    }
 
     /**
-     * @Route("/recettes", name="recette_list" )
+     * @Route("/ajax/recettes", name="ajax_search" )
      */
     public function ajaxBar(Request $request, RecetteRepository $recetteRepository)
     {
 
-            // if ($request != null){
-            // $query = $request->query->get('query');
 
-            // $recettes = $recetteRepository->findByRequest($query);
+            if($request->get('query'))
+            {
+                $query = $request->get('query');
+                $response = $recetteRepository->findByRequest($query);
+                $output = '<ul class="dropdown-menu"
+                    style= "display:block;
+                    position:relative">';
+                foreach($response as $row)
+                {
+                    $output .= '<li> <a href="#">' .$row->getTitle(). '</a> </li>';
+                }
+                $output .= '</ul>';
 
-            // $recetteList = '<ul id="matchList">';
-
-            $data = $request->get('query');
-            $em =$this->getDoctrine()->getManager();
-            $query = $em->createQuery(
-                'SELECT r
-                FROM App\Entity\Recette r
-                WHERE r.title LIKE :data
-                ORDER BY r.title ASC'
-            )
-            ->setParameter('data', '%'.$data.'%');
-            $results = $query->getResult();
-
-            $recetteList = '<ul id="matchList">';
-
-            foreach ($results as $result) {
-                $matchStringBold = preg_replace('/('.$data.')/i', '<strong>$1</strong>', $result['title']); // Replace text field input by bold one
-                $recetteList .= '<li id="'.$result['title'].'">'.$matchStringBold.'</li>'; // Create the matching list - we put maching name in the ID too
+                return new JsonResponse( array(
+                    'status' => true,
+                    'content' => $output,
+                ));
             }
-            $recetteList .= '</ul>';
 
-            $response = new JsonResponse();
-            $response->setData(array('recetteList' => $recetteList));
-
-            return $this->render('view/list.html.twig');
+            return new JsonResponse( array(
+                'status' => false
+            ));
         }
 }
 
